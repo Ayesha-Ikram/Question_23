6dfe3809b453e0a0007f11643424d097b9bed6d1
Ayesha-Ikram <160381040+Ayesha-Ikram@users.noreply.github.com>
2024-03-06 20:25:52 +0500

    Add files via upload

---
+++ Conditional_Test.ts
@@ -0,0 +1,50 @@
+let fullName : string = "Ayesha Ikram";
+let fatherName : string ="Ikram_ud_din";
+let age : number = 24;
+let isFollow : boolean =true;
+let numberList : number[] =[12345];
+let emptystring :string = "";
+let education : string = "Bachlore's";
+let experience_years : number = 2;
+let location_ : string ="Karachi";
+let isRaining : boolean = false;
+
+// 1st Statement
+console.log("Is fullName == 'Ayesha Ikram' ? I predict true" );
+console.log(fullName == 'Ayesha Ikram');
+
+//2nd Statement 
+console.log("Is fatherName == ' Ikram_ud_din' ? I predict true" );
+console.log(fatherName =='Ikram_ud_din');
+
+//3rd Statement
+console.log("Is age > 18  ? I predict true" );
+console.log(age > 18);
+
+//4th Statement
+console.log("Is isFollow ==true  ? I predict true" );
+console.log(isFollow ==true);
+
+//5th Statement
+console.log("Is numberList.length>0 ? I predict true" );
+console.log(numberList.length>0);
+
+//6th Statement
+console.log("Is emptystring =='abc' ? I predict false" );
+console.log(emptystring =='abc');
+
+//7th Statement
+console.log("Is education=='Master'? I predict false" );
+console.log(education =='Master');
+
+//8th Statement
+console.log("Is experience_years==4 ? I predict false" );
+console.log(experience_years == 4);
+
+//9th Statement
+console.log("Is location_=='Islamabad' ? I predict false" );
+console.log(location_=='Islamabad' );
+
+//10th Statement
+console.log("Is isRaining =='false'? I predict false" );
+console.log(isRaining == false);
